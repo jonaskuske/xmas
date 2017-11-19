6d6392c892e060a442feef7d40342f2ae4f34529
Jonas <30421456+jonaskuske@users.noreply.github.com>
2017-11-19 21:55:43 +0100

    navigation implemented

--- src/main.js
+++ src/main.js
@@ -4,16 +4,44 @@ import '@sounds/xmas';
 import particles from 'particlesjs';
 import config from './particles.config';
 
-const $ = val => val.startsWith('#') ? document.querySelector(val) : document.querySelectorAll(val);
+const $ = val => val.startsWith('#')
+  ? document.querySelector(val)
+  : document.querySelectorAll(val);
+const create = el => document.createElement(el);
+
 let audioIsPlaying = true;
 
 document.addEventListener('DOMContentLoaded', () => {
   injectDoors();
   injectAudio();
+  attachListeners();
   particles.init(config);
+  $('body')[0].classList.remove('invisible');
+});
 
+function injectDoors() {
+  let days = Array.from(Array(24).keys()).map(val => val + 1); //create Array with entries 1-24
+  shuffle(days);
+  days.forEach(day => {
+    const door = create('div');
+    door.className += 'door';
+    door.setAttribute('data-day', day);
+    door.innerHTML = `<p class="date">${day}</p>`;
+    door.addEventListener('click', navigateToGiftView);
+    $('.door-wrapper')[0].append(door);
+  });
+}
+function injectAudio() {
+  const audio = create('audio');
+  audio.id = 'audio-player';
+  audio.src = './sound/xmas.mp3';
+  audio.autoplay = true;
+  audio.loop = true;
+  $('body')[0].append(audio);
+}
+function attachListeners() {
   $('#shuffle-btn').addEventListener('click', () => {
-    $('#calendar').innerHTML = '';
+    $('.door-wrapper')[0].innerHTML = '';
     injectDoors();
   });
   $('#mute-btn').addEventListener('click', () => {
@@ -22,29 +50,19 @@ document.addEventListener('DOMContentLoaded', () => {
       : $('#audio-player').play();
     audioIsPlaying = !audioIsPlaying;
   });
-});
-
+  $('#back-btn').addEventListener('click', () => {
+    $('.gift-wrapper')[0].classList.add('hidden');
+    $('.door-wrapper')[0].classList.remove('hidden');
+  });
+}
 function shuffle(array) {
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [array[i], array[j]] = [array[j], array[i]];
   }
 }
-function injectDoors() {
-  let days = Array.from(Array(24).keys()).map(val => val + 1);
-  shuffle(days);
-  days.forEach(day => {
-    const door = document.createElement('div');
-    door.className += 'door';
-    door.innerHTML = `<p class="date">${day}</p>`;
-    $('#calendar').append(door);
-  });
-}
-function injectAudio() {
-  const audio = document.createElement('audio');
-  audio.id = 'audio-player';
-  audio.src = './sound/xmas.mp3';
-  audio.autoplay = true;
-  audio.loop = true;
-  $('body')[0].append(audio);
+function navigateToGiftView(evt) {
+  $('#gift-view-date').innerHTML = evt.target.dataset.day;
+  $('.door-wrapper')[0].classList.add('hidden');
+  $('.gift-wrapper')[0].classList.remove('hidden');
 }
