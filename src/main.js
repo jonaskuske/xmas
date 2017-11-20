1de188a8f10c5b107d9e2b4620d6626be8229402
Jonas <30421456+jonaskuske@users.noreply.github.com>
2017-11-20 01:19:31 +0100

    better styling + code more concise

--- src/main.js
+++ src/main.js
@@ -1,68 +1,67 @@
 import '@styles/main-style';
 
 import '@sounds/xmas';
-import particles from 'particlesjs';
+import particlesJS from 'particlesjs';
 import config from './particles.config';
 
-const $ = val => val.startsWith('#')
-  ? document.querySelector(val)
-  : document.querySelectorAll(val);
-const create = el => document.createElement(el);
+// 'my little jQuery' :D
+const $ = selector => selector.startsWith('#')
+  ? document.querySelector(selector)
+  : document.querySelectorAll(selector);
+const create = node => document.createElement(node);
+Object.prototype.addClass = function (_class) { this.classList.add(_class); return this; };
+Object.prototype.removeClass = function (_class) { this.classList.remove(_class); return this; };
+Object.prototype.attr = function (attr, val) { this.setAttribute(attr, val); return this; };
+Object.prototype.txt = function (txt) { this.textContent = txt; return this; };
+Object.prototype.on = function (evt, callback, options) { this.addEventListener(evt, callback, options); return this; };
+Object.prototype.empty = function () { while (this.lastChild) { this.removeChild(this.lastChild); } };
+//
 
-let audioIsPlaying = true;
+const passive = { passive: true };
+let audioIsPlaying = false;
+const calendar = $('.door-wrapper')[0];
+const content = $('.gift-wrapper')[0];
 
-document.addEventListener('DOMContentLoaded', () => {
+document.on('DOMContentLoaded', () => {
   injectDoors();
-  injectAudio();
   attachListeners();
-  particles.init(config);
-  $('body')[0].classList.remove('invisible');
+  let particles = particlesJS.init(config);
+  $('#audio-player').on('play', () => { audioIsPlaying = true; });
 });
-
 function injectDoors() {
   let days = Array.from(Array(24).keys()).map(val => val + 1); //create Array with entries 1-24
   shuffle(days);
   days.forEach(day => {
-    const door = create('div');
-    door.className += 'door';
-    door.setAttribute('data-day', day);
-    door.innerHTML = `<p class="date">${day}</p>`;
-    door.addEventListener('click', navigateToGiftView);
-    $('.door-wrapper')[0].append(door);
+    const door = create('div').addClass('door').attr('data-day', day).on('click', navigateToGiftView, passive);
+    const date = create('p').addClass('date').txt(day);
+    door.append(date);
+    calendar.append(door);
   });
-}
-function injectAudio() {
-  const audio = create('audio');
-  audio.id = 'audio-player';
-  audio.src = './sound/xmas.mp3';
-  audio.autoplay = true;
-  audio.loop = true;
-  $('body')[0].append(audio);
 }
 function attachListeners() {
-  $('#shuffle-btn').addEventListener('click', () => {
-    $('.door-wrapper')[0].innerHTML = '';
+  $('#shuffle-btn').on('click', () => {
+    calendar.empty();
     injectDoors();
-  });
-  $('#mute-btn').addEventListener('click', () => {
+  }, passive);
+  $('#mute-btn').on('click', () => {
     audioIsPlaying
       ? $('#audio-player').pause()
       : $('#audio-player').play();
     audioIsPlaying = !audioIsPlaying;
-  });
-  $('#back-btn').addEventListener('click', () => {
-    $('.gift-wrapper')[0].classList.add('hidden');
-    $('.door-wrapper')[0].classList.remove('hidden');
-  });
+  }, passive);
+  $('#back-btn').on('click', () => {
+    content.addClass('hidden');
+    calendar.removeClass('hidden');
+  }, passive);
+}
+function navigateToGiftView() {
+  $('#gift-view-date').txt(this.dataset.day);
+  calendar.addClass('hidden');
+  content.removeClass('hidden');
 }
 function shuffle(array) {
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [array[i], array[j]] = [array[j], array[i]];
   }
-}
-function navigateToGiftView(evt) {
-  $('#gift-view-date').innerHTML = evt.target.dataset.day;
-  $('.door-wrapper')[0].classList.add('hidden');
-  $('.gift-wrapper')[0].classList.remove('hidden');
 }
